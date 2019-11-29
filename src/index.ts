bdaadc583402fff1be2c7812a58ac46694d4870f
huocs <huo8008@126.com>
2019-11-29 15:54:39 +0800

    通过 XMLHttpRequest 发送请求

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 
 import {AxiosRequestConfig} from './types'
+import xhr from './xhr'
 // 指定 config 类型
-function axios(config: AxiosRequestConfig) {
-
+function axios(config: AxiosRequestConfig): void {
+  xhr(config)
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,7 +1,11 @@
 // 实现所有请求逻辑
 
-import {AxiosRequestConfig} from './types'
+import { AxiosRequestConfig } from './types'
 
-export default function xhr(config: AxiosRequestConfig) {
-
+export default function xhr(config: AxiosRequestConfig): void {
+  const { data = null, url, method = 'get' } = config
+  const request = new XMLHttpRequest()
+  // 默认异步
+  request.open(method.toUpperCase(), url, true)
+  request.send(data)
 }
