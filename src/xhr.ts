c6168c4b9142e2b66454162519127cd23af8bfa6
huocs <huo8008@126.com>
2019-11-30 16:10:27 +0800

    网络请求异溃、超时、及其他错误处理

--- src/xhr.ts
+++ src/xhr.ts
@@ -5,16 +5,22 @@ import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
     if (responseType) {
       request.responseType = responseType
+    }
+    if (timeout) {
+      request.timeout = timeout
     }
     // 默认异步
     request.open(method.toUpperCase(), url, true)
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+      if (request.status === 0) {
         return
       }
       const reponseHeaders = parseHeaders(request.getAllResponseHeaders())
@@ -27,7 +33,15 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
+    }
+    // 网络请求异常处理
+    request.onerror = function handleError() {
+      reject(new Error('Network Error'))
+    }
+    // 超时处理
+    request.ontimeout = function handleTimeout() {
+      reject(new Error(`Timeout of ${timeout} ms exceeeded`))
     }
     Object.keys(headers).forEach(name => {
       if (data === null && name.toLocaleLowerCase() === 'content-type') {
@@ -37,5 +51,12 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     })
     request.send(data)
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(new Error(`Request failed with status code ${response.status}`))
+      }
+    }
   })
 }
