4d39ad1cba6ccdbb71a71ff3174d5b767881dec5
huocs <huo8008@126.com>
2019-11-30 11:36:48 +0800

    修改错误

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,7 +1,7 @@
 import { isDate, isObject } from './util'
 
 function encode(val: string): string {
-  return encodeURLComponent(val)
+  return encodeURIComponent(val)
     .replace(/%40/g, '@')
     .replace(/%3A/gi, ':')
     .replace(/%24/g, '$')
