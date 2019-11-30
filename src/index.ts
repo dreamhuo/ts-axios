076f3a80639938f123eabf389aebfb0e27d50996
huocs <huo8008@126.com>
2019-11-30 11:20:02 +0800

    主函数调用对 config 进行处理

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-
-import {AxiosRequestConfig} from './types'
+import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 // 指定 config 类型
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+// 在发送请求之前，对 config 进行一层处理
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+// 对 config 的 url 做处理
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
