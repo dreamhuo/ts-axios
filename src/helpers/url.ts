34d88f68bc182087ec141bbed9818102d5d7788f
huocs <huo8008@126.com>
2019-11-30 11:13:00 +0800

    对url 参数进行处理，buildURL实现

---
+++ src/helpers/url.ts
@@ -0,0 +1,52 @@
+import { isDate, isObject } from './util'
+
+function encode(val: string): string {
+  return encodeURLComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any) {
+  if (!params) {
+    return url
+  }
+  const parts: string[] = []
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    // 若当前为 null 或 undefined 则跳出当前循环
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      // 把参数做成键值对
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+  return url
+}
