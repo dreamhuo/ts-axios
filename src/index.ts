4795b8939b1753484914f780f1338d72aa5233f2
huocs <huo8008@126.com>
2019-11-30 18:16:56 +0800

    代码输出优化

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
+import axios from './axios'
 
-// 指定 config 类型
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-// 在发送请求之前，对 config 进行一层处理
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transforHeaders(config)
-  config.data = transformRequestData(config)
-}
-// 对 config 的 url 做处理
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-// 对 Request data 进行处理
-function transformRequestData(config: AxiosRequestConfig) {
-  return transformRequest(config.data)
-}
-// 对 headers 进行处理
-function transforHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-// 对data进行处理
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+export * from './types'
 
 export default axios
