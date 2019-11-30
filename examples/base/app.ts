c053437da0f83a2e7139c19e316b3116902bdfed
huocs <huo8008@126.com>
2019-11-30 12:15:36 +0800

    请求 data  处理

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,66 +1,66 @@
 import axios from '../../src/index'
 
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: ['bar', 'baz']
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: {
-      bar: 'baz'
-    }
-  }
-})
-
-const date = new Date()
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    date
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: '@:$, '
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get',
-  params: {
-    foo: 'bar',
-    baz: null
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get#hash',
-  params: {
-    foo: 'bar'
-  }
-})
-
-axios({
-  method: 'get',
-  url: '/base/get?foo=bar',
-  params: {
-    bar: 'baz'
-  }
-})
-
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: ['bar', 'baz']
+//   }
+// })
+//
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: {
+//       bar: 'baz'
+//     }
+//   }
+// })
+//
+// const date = new Date()
+//
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     date
+//   }
+// })
+//
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: '@:$, '
+//   }
+// })
+//
+// axios({
+//   method: 'get',
+//   url: '/base/get',
+//   params: {
+//     foo: 'bar',
+//     baz: null
+//   }
+// })
+//
+// axios({
+//   method: 'get',
+//   url: '/base/get#hash',
+//   params: {
+//     foo: 'bar'
+//   }
+// })
+//
+// axios({
+//   method: 'get',
+//   url: '/base/get?foo=bar',
+//   params: {
+//     bar: 'baz'
+//   }
+// })
+//
 axios({
   method: 'post',
   url: '/base/post',
@@ -82,13 +82,13 @@ axios({
 //   }
 // })
 //
-// const arr = new Int32Array([21, 31])
-//
-// axios({
-//   method: 'post',
-//   url: '/base/buffer',
-//   data: arr
-// })
+const arr = new Int32Array([21, 31])
+
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
 //
 //
 // const paramsString = 'q=URLUtils.searchParams&topic=api'
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
+
 // 指定 config 类型
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -9,11 +11,16 @@ function axios(config: AxiosRequestConfig): void {
 // 在发送请求之前，对 config 进行一层处理
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
+  config.data = transformRequestData(config)
 }
 // 对 config 的 url 做处理
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+// 对 Request data 进行处理
+function transformRequestData(config: AxiosRequestConfig) {
+  return transformRequest(config.data)
 }
 
 export default axios
