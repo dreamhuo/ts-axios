7d034db79220e0d673fe296fc5f2d66ac5206e3c
huocs <huo8008@126.com>
2019-12-02 19:52:06 +0800

    request 参数重载实现

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -2,7 +2,16 @@ import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
 import dispatchRequest from './dispatchRequest'
 
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
+  request(url: any, config?: any): AxiosPromise {
+    // 参数重载
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
