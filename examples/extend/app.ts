fb934fb5a8eab4d1358037ca48b53db8c40fe5bd
huocs <huo8008@126.com>
2019-12-02 20:16:48 +0800

    响应数据支持泛型数据，接口添加泛型参数

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,34 +1,34 @@
 import axios from '../../src/index'
-
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
-  }
-})
-
-axios.request({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
-
-axios.get('/extend/get')
-
-axios.options('/extend/options')
-
-axios.delete('/extend/delete')
-
-axios.head('/extend/head')
-
-axios.post('/extend/post', { msg: 'post' })
-
-axios.put('/extend/put', { msg: 'put' })
-
-axios.patch('/extend/patch', { msg: 'patch' })
+//
+// axios({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hi'
+//   }
+// })
+//
+// axios.request({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
+//
+// axios.get('/extend/get')
+//
+// axios.options('/extend/options')
+//
+// axios.delete('/extend/delete')
+//
+// axios.head('/extend/head')
+//
+// axios.post('/extend/post', { msg: 'post' })
+//
+// axios.put('/extend/put', { msg: 'put' })
+//
+// axios.patch('/extend/patch', { msg: 'patch' })
 
 // axios({
 //   url: '/extend/post',
@@ -45,29 +45,32 @@ axios.patch('/extend/patch', { msg: 'patch' })
 //   }
 // })
 //
-// interface ResponseData<T = any> {
-//   code: number
-//   result: T
-//   message: string
-// }
-//
-// interface User {
-//   name: string
-//   age: number
-// }
-//
-// function getUser<T>() {
-//   return axios<ResponseData<T>>('/extend/user')
-//     .then(res => res.data)
-//     .catch(err => console.error(err))
-// }
-//
-//
-// async function test() {
-//   const user = await getUser<User>()
-//   if (user) {
-//     console.log(user.result.name)
-//   }
-// }
-//
-// test()
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+// 把接口数据类型传入的好处是：
+// 可以优先定义好接口的数据类型
+// 在调用请求时，把数据类型传入，在响应时可以正确的拿到对应的结果的类型
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test()
