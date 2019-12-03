28580c42d73d39def4e0f25401978463e1c58947
huocs <huo8008@126.com>
2019-12-03 14:24:34 +0800

    拦截器管理类和接口定义

---
+++ src/core/InterceptorManager.ts
@@ -0,0 +1,37 @@
+import { ResolvedFn, RejectedFn } from '../types'
+
+interface Interceptor<T> {
+  resolved: ResolvedFn<T>
+  rejected?: RejectedFn
+}
+
+export default class InterceptorManager<T> {
+  private interceptors: Array<Interceptor<T> | null>
+
+  constructor() {
+    this.interceptors = []
+  }
+
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number {
+    this.interceptors.push({
+      resolved,
+      rejected
+    })
+    return this.interceptors.length - 1
+  }
+
+  forEach(fn: (interceptor: Interceptor<T>) => void): void {
+    this.interceptors.forEach(interceptor => {
+      if (interceptor !== null) {
+        fn(interceptor)
+      }
+    })
+  }
+
+  eject(id: number): void {
+    if (this.interceptors[id]) {
+      // 这里不能直接删除，直接删除会导致 id 错乱
+      this.interceptors[id] = null
+    }
+  }
+}
