d98727df19cd94d917a697d0ec7d1bb3db10b242
huocs <huo8008@126.com>
2019-12-06 20:02:57 +0800

    transform 优化

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -1,8 +1,8 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
 import xhr from './xhr'
 import { buildURL } from '../helpers/url'
-import { transformRequest, transformResponse } from '../helpers/data'
-import { processHeaders, flattenHeaders } from '../helpers/headers'
+import { flattenHeaders } from '../helpers/headers'
+import transform from './transform'
 
 // 指定 config 类型
 export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
@@ -14,27 +14,19 @@ export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromis
 // 在发送请求之前，对 config 进行一层处理
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
-  config.headers = transforHeaders(config)
-  config.data = transformRequestData(config)
+  config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
+
 // 对 config 的 url 做处理
 function transformURL(config: AxiosRequestConfig): string {
   const { url, params } = config
   // 这里类型断言不为空，加 ！号
   return buildURL(url!, params)
 }
-// 对 Request data 进行处理
-function transformRequestData(config: AxiosRequestConfig) {
-  return transformRequest(config.data)
-}
-// 对 headers 进行处理
-function transforHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
+
 // 对data进行处理
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
+  res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
 }
